9173b6d396aba70912cb9ddc64f20bea9868efeb
josiassantos15 <josiassantos1577@gmail.com>
2025-06-19 19:35:50 -0300

    feat: first commit

---
+++ src/main/java/com/josiassantos/desafio_anota_ai/DesafioAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.josiassantos.desafio_anota_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAnotaAiApplication.class, args);
+	}
+
+}
